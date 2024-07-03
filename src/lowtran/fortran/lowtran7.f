a11f7ea56e40d226d592c8dd3b2dafcd5f7c5247
(batg) Battaglia Mattia <batg@zhaw.ch>
2024-07-03 16:25:04 +0200

    Revert "changes 13 in model height"
    This reverts commit 8e2705b11c4fdb8fc3bd789a7171731914512539.

--- src/lowtran/fortran/lowtran7.f
+++ src/lowtran/fortran/lowtran7.f
@@ -1917,7 +1917,8 @@ C
           WRITE(IPR,81)ZMDL(K),P(K),T(K),WMOL(1),WMOL(2),WMOL(3),
      X     (JCHAR(KM),KM=1,14)
 81         FORMAT ( F10.3,1P5E10.3,10X,15A1)
-
+            else
+                ZMDL(K) = ZM(K)
             ENDIF
       ENDIF
       IF(IRD1 .EQ. 1) THEN
